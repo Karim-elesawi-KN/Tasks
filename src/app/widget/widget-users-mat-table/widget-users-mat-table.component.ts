b21a98690fb106e819a6f3c0069af2aebd88e36c
Karim-elesawi-KN <khany@kn-it.com>
2024-09-03 12:15:18 +0300

    slide up filter in mat table

--- src/app/widget/widget-users-mat-table/widget-users-mat-table.component.ts
+++ src/app/widget/widget-users-mat-table/widget-users-mat-table.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
@@ -7,6 +14,9 @@ import {
   MatCheckboxChange,
 } from '@angular/material/checkbox';
 import { WidgetService } from '../../services/widget.service';
+import { CommonModule } from '@angular/common';
+import { Subject, debounceTime } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-widget-users-mat-table',
@@ -16,61 +26,110 @@ import { WidgetService } from '../../services/widget.service';
     MatTableModule,
     MatPaginatorModule,
     MatCheckboxModule,
+    CommonModule,
+    FormsModule,
   ],
   templateUrl: './widget-users-mat-table.component.html',
   styleUrls: ['./widget-users-mat-table.component.scss'],
 })
 export class WidgetUsersMatTableComponent implements OnInit {
   displayedColumns: string[] = ['select', 'ID', 'name', 'country', 'age'];
-  dataSource = new MatTableDataSource<any>([]); 
+  dataSource = new MatTableDataSource<any>([]);
 
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   selection = new Set<any>();
+  private filterSubject: Subject<void> = new Subject<void>();
 
-  constructor(private widgetService: WidgetService) {}
+  constructor(private widgetService: WidgetService) {
+    this.filterSubject.pipe(debounceTime(300)).subscribe(() => {
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    });
+  }
 
   ngOnInit(): void {
     this.loadUserData();
+    this.dataSource.filterPredicate = this.customFilterPredicate();
   }
-
   async loadUserData(): Promise<void> {
     await this.widgetService.loadUsers();
-    this.dataSource = new MatTableDataSource(this.widgetService.getUsers());
-    this.dataSource.sort = this.sort; // Ensure sort is assigned after data is loaded
+    const users = this.widgetService.getUsers();
+    this.dataSource.data = users;
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   toggleAllSelection(event: MatCheckboxChange) {
     if (event.checked) {
       this.dataSource.data.forEach((user: any) => this.selection.add(user));
     } else {
-      this.selection.clear();                 
+      this.selection.clear();
     }
   }
-
   isAllSelected(): boolean {
     return (
       this.dataSource.data.length > 0 &&
       this.dataSource.data.every((user: any) => this.selection.has(user))
     );
   }
-
   isSomeSelected(): boolean {
     return this.selection.size > 0 && !this.isAllSelected();
   }
-
   onRowCheckboxChange(event: MatCheckboxChange, user: any) {
     if (event.checked) {
       this.selection.add(user);
     } else {
       this.selection.delete(user);
     }
+  }
+
+  @Input() isFilterMode = false;
+  @Output() filterToggle = new EventEmitter<boolean>();
+  filterValues: any = {
+    ID: '',
+    name: '',
+    country: '',
+    age: '',
+  };
+  toggleFilter() {
+    this.isFilterMode = !this.isFilterMode;
+    if (!this.isFilterMode) {
+      this.filterValues = {
+        ID: '',
+        name: '',
+        country: '',
+        age: '',
+      };
+      this.dataSource.filter = '';
+    }
+  }
+  applyFilter(column: string, event: any) {
+    const filterValue = event.target.value.trim().toLowerCase();
+    this.filterValues[column] = filterValue;
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+  }
+
+  customFilterPredicate(): (data: any, filter: string) => boolean {
+    return (data, filter): boolean => {
+      const filterObject = JSON.parse(filter);
+      const matchesID = data.ID.toLowerCase().includes(
+        filterObject.ID.toLowerCase()
+      );
+      const matchesName = data.name
+        .toLowerCase()
+        .includes(filterObject.name.toLowerCase());
+      const matchesCountry = data.country
+        .toLowerCase()
+        .includes(filterObject.country.toLowerCase());
+      const matchesAge = data.age.toString().includes(filterObject.age);
+
+      return (
+        (!filterObject.ID || matchesID) &&
+        (!filterObject.name || matchesName) &&
+        (!filterObject.country || matchesCountry) &&
+        (!filterObject.age || matchesAge)
+      );
+    };
   }
 }
